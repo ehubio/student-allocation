3fb9db2bb5c3b48db68a4dc52079ccc8031b8d04
Rob Ashton <r.ashton@imperial.ac.uk>
2024-03-22 19:20:16 +0000

    Initialise project and static site

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from 'astro/config';
+import vue from '@astrojs/vue';
+
+// https://astro.build/config
+export default defineConfig({
+	// Enable Vue to support Vue components.
+	integrations: [vue()],
+	site: 'https://ehubio.github.io',
+	base: 'student-allocation',
+});
